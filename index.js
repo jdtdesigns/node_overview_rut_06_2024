f88b8abea0d1724cb0be52dd0acf8d907ac16a0b
JD Tadlock <jt7903@gmail.com>
2024-07-17 12:50:04 -0400

    finished basic class conversion

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
-// Object Literal
-const user = {
-  name: 'JD',
-  age: 44,
-  hobbies: ['pickleball', 'fishing'],
-  getAge: function () {
+class User {
+  constructor(name, age, hobbies) {
+    this.name = name;
+    this.age = age;
+    this.hobbies = hobbies;
+  }
+
+  getAge() {
     return this.age;
-  },
-  haveBirthday: function () {
+  }
+
+  haveBirthday() {
     this.age++;
   }
-};
-
-// user.haveBirthday();
-
-
-// Constructor Function - Generates Objects
-function User(name, age, hobbies) {
-  this.name = name;
-  this.age = age;
-  this.hobbies = hobbies;
-}
-
-User.prototype.species = 'human';
-
-User.prototype.getAge = function () {
-  return this.age;
-}
-
-User.prototype.haveBirthday = function () {
-  this.age++;
 }
 
 const jd = new User('JD', 44, ['pickleball', 'fishing']);
 const bob = new User('Bob', 99, ['bingo', 'sit on the porch']);
 
-
-console.log(jd.species);
 console.log(bob);
-// console.log(bob.getAge());
-// jd.haveBirthday();
+
