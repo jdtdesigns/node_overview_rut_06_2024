2d60848b740dffa0eb8f10b66c776c3d9b30656e
JD Tadlock <jt7903@gmail.com>
2024-07-17 14:02:58 -0400

    covered inheritance and polymorphism

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-class User {
-  constructor(name, age, hobbies) {
-    this.name = name;
-    this.age = age;
-    this.hobbies = hobbies;
+class Vehicle {
+  constructor(tireAmount, color) {
+    this.tireAmount = tireAmount;
+    this.color = color;
   }
 
-  getAge() {
-    return this.age;
-  }
-
-  haveBirthday() {
-    this.age++;
+  getTireAmount() {
+    return this.tireAmount;
   }
 }
 
-const jd = new User('JD', 44, ['pickleball', 'fishing']);
-const bob = new User('Bob', 99, ['bingo', 'sit on the porch']);
+class Bike extends Vehicle {
+  constructor(tireAmount, color, brakeSystem, gears) {
+    super(tireAmount, color);
 
-console.log(bob);
+    this.brakeSystem = brakeSystem;
+    this.gears = gears;
+  }
+}
 
+class Motorcycle extends Vehicle {
+  constructor(tireAmount, color, engine, type) {
+    super(tireAmount, color);
+
+    this.engine = engine;
+    this.type = type;
+  }
+
+  getTireAmount() {
+    return this.tireAmount + ' brake pad';
+  }
+}
+
+const rad = new Bike(1, 'green', 'Shimano', 7);
+const ninja = new Motorcycle(2, 'red', 'cool', 'dirt');
+
+const radTireAmount = rad.getTireAmount();
+const ninjaTireAmount = ninja.getTireAmount();
+
+console.log(radTireAmount);
+console.log(ninjaTireAmount);
